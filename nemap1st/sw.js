27ebf24007e9d16913184a3c1ad583f360ee93ad
Tyler <tyler-franklin@outlook.com>
2026-07-14 10:32:26 -0400

    Add files via upload

---
+++ nemap1st/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'ne-1st-cache-v1';
+
+// List all the files you want cached
+const urlsToCache = [
+    './',
+    './index.html',
+    './ne1st.png',
+    './nelogo.png'
+];
+
+// Install the service worker and open the cache
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Intercept network requests and serve from cache if offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // If the file is in the cache, return it. Otherwise, fetch it from the network.
+                return response || fetch(event.request);
+            })
+    );
+});
