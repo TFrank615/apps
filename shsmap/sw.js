a01de4dd6f00a21151f9f47e73edca262178efc4
Tyler <tyler-franklin@outlook.com>
2026-07-16 10:55:33 -0400

    Add files via upload

---
+++ shsmap/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'shs-map-cache-v1';
+
+// List all files, including your new map image
+const urlsToCache = [
+    './',
+    './index.html',
+    './shs1st.png',
+    './shs2nd.png', // Added second image
+    './shsbasement.png', // Added second image
+    './shslogo.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
